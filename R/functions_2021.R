5fa2f3d655a669c4172265b780a736e96ffaa73b
Gabriele Tomè <tome.gabriele@gmail.com>
2023-04-04 15:59:25 +0200

    New interactive vulcano plot

--- R/functions_2021.R
+++ R/functions_2021.R
@@ -1351,3 +1351,51 @@ kinase_act_phosr <- function(dirOutput_kinase, formule_CORAL, comp, dat_pep, dep
   
   renderSvgPanZoom(comp, kinase_Act, dirOutput_kinase)
 }
+
+
+
+### Knit Tab for figure ----
+
+in_tabs <- function(l, labels = names(l), level, knit = TRUE, close_tabset = FALSE) {                                                  
+  if(is.null(labels)) {                                                                                                              
+    stop("labels are NULL, it is required not to be so that the tabs have proper names")                                           
+  }                                                                                                                                  
+  names(l) <- labels                                                                                                                 
+  rmd_code <- lapply(seq_along(l), FUN = function(i) obj_to_rmd(l[[i]], name = names(l)[i], level = level + 1L))                     
+  if(isTRUE(getOption("knitr.in.progress"))) {                                                                                       
+    res <- knitr::knit(text = unlist(rmd_code), quiet = TRUE)                                                                      
+    cat(res)                                                                                                                       
+  } else {                                                                                                                           
+    if(!knit) {                                                                                                                    
+      cat(unlist(rmd_code))                                                                                                      
+    } else {                                                                                                                       
+      return(l)                                                                                                                  
+    }                                                                                                                              
+  }                                                                                                                                  
+  if(close_tabset) {                                                                                                                 
+    cat(paste(get_section(level), "{.unlisted .unnumbered .toc-ignore .tabset}", "\n"))                                            
+  }                                                                                                                                  
+}                                                                                                                                      
+
+get_section <- function(level) {                                                                                                       
+  paste(rep("#", times = level), collapse = "")                                                                                      
+}                                                                                                                                      
+
+get_tabset <- function(obj) {                                                                                                          
+  ifelse(inherits(obj, "list"), "{.tabset}", "")                                                                                     
+}                                                                                                                                      
+
+obj_to_rmd <- function(obj, parent_name = "l", name, level) {                                                                          
+  section_code <- sprintf("%s %s %s\n", get_section(level), name, get_tabset(obj))                                                   
+  if(!inherits(obj, "list")) {                                                                                                       
+    rmd_code <- c("```{r, echo = FALSE}\n",                                                                                    
+                  sprintf("%s$`%s`\n", parent_name, name),                                                                     
+                  "```\n",                                                                                                     
+                  "\n")                                                                                                        
+  } else {                                                                                                                           
+    rmd_code <- c("\n",                                                                                                            
+                  lapply(X = seq_along(obj),                                                                                       
+                         FUN = function(i) obj_to_rmd(obj[[i]], sprintf("%s$`%s`", parent_name, name), names(obj)[i], level + 1L)))
+  }                                                                                                                                  
+  return(c(section_code, rmd_code))                                                                                                  
+} 
